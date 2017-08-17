d984bfc681c2a70df49f8b580c13d4d16db1b9f9
joezhangls <ohoh0623@gmail.com>
2017-08-17 22:37:52 +0800

    Update Mseeages.cs

--- GmailViewer/Models/Mseeages.cs
+++ GmailViewer/Models/Mseeages.cs
@@ -1,37 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using Google.Apis.Auth.OAuth2;
 using Google.Apis.Gmail.v1;
 using Google.Apis.Gmail.v1.Data;
 using Google.Apis.Services;
 using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text;
 using System.Threading;
-using System.Web.Hosting;
+using System.Threading.Tasks;
 
-namespace GmailViewer.Models
+namespace gapitest
 {
-    public class Mseeages
+    class Program
     {
-        string[] Scopes = { GmailService.Scope.GmailReadonly };
-        string ApplicationName = "Gmail API .NET Quickstart";
+        static string[] Scopes = { GmailService.Scope.GmailReadonly };
+        static string ApplicationName = "Gmail API .NET Quickstart";
 
-        public List<gMessage> ListMinimal { get; set; }
-
-        public void getlist()
+        static void Main(string[] args)
         {
-            ListMinimal = new List<gMessage>();
             UserCredential credential;
 
             using (var stream =
-                new FileStream(HostingEnvironment.MapPath("~/App_Data/client_secret_50186485062-qtg42d1kc2vldvanujspfolg783ke3on.apps.googleusercontent.com.json"), FileMode.Open, FileAccess.Read))
+                new FileStream("client_secret_50186485062-qtg42d1kc2vldvanujspfolg783ke3on.apps.googleusercontent.com.json", FileMode.Open, FileAccess.Read))
             {
                 string credPath = System.Environment.GetFolderPath(
                     System.Environment.SpecialFolder.Personal);
                 credPath = Path.Combine(credPath, ".credentials/gmail-dotnet-quickstart.json");
-                credPath = HostingEnvironment.MapPath("~/App_Data/.credentials/gmail-dotnet-quickstart.json");
+
                 credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
                     GoogleClientSecrets.Load(stream).Secrets,
                     Scopes,
@@ -49,6 +46,22 @@ namespace GmailViewer.Models
             });
 
             // Define parameters of request.
+            //UsersResource.LabelsResource.ListRequest request = service.Users.Labels.List("me");
+
+            //// List labels.
+            //IList<Label> labels = request.Execute().Labels;
+            //Console.WriteLine("Labels:");
+            //if (labels != null && labels.Count > 0)
+            //{
+            //    foreach (var labelItem in labels)
+            //    {
+            //        Console.WriteLine("{0}", labelItem.Name);
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No labels found.");
+            //}
             var messageRequest = service.Users.Messages.List("me");
             IList<Message> messages = messageRequest.Execute().Messages;
             // List messages.
@@ -58,29 +71,26 @@ namespace GmailViewer.Models
                 foreach (var labelItem in messages)
                 {
                     var item = service.Users.Messages.Get("me", labelItem.Id);
-                    item.Format = UsersResource.MessagesResource.GetRequest.FormatEnum.Full;
+                    item.Format = UsersResource.MessagesResource.GetRequest.FormatEnum.Metadata;
+
                     var _item = item.Execute();
-                    string subject = _item.Payload.Headers.FirstOrDefault(x => x.Name == "Subject").Value;
-                    ListMinimal.Add(new gMessage()
+                    string subject = _item.Payload.Headers.FirstOrDefault(x => x.Name == "Subject").Value; //"INBOX"UNREAD
+                    DateTime _getdate = Convert.ToDateTime(_item.Payload.Headers.FirstOrDefault(x => x.Name == "Date").Value);
+                    string _From = _item.Payload.Headers.FirstOrDefault(x => x.Name == "From").Value;
+                    if(_From.Split('\"').Length > 2)
                     {
-                        getdate = DateTime.Now.ToString("yyyy/MM/dd HH:mm"),
-                        subject = subject,
-                        id = _item.Id
-                    });
+                        _From = _item.Payload.Headers.FirstOrDefault(x => x.Name == "From").Value.Split('\"')[1];
+                    }
+                    Console.WriteLine("{0} : {1} : {2} : {3}", _From, subject, _getdate.ToString("yyyy/MM/dd hh:mm"), string.Join(",", _item.LabelIds));
                 }
             }
             else
             {
                 Console.WriteLine("No labels found.");
             }
+
+            Console.Read();
+
         }
-    }
-
-    public class gMessage
-    {
-        public string id { get; set; }
-        public string subject { get; set; }
-        public string getdate { get; set; }
-
     }
 }
